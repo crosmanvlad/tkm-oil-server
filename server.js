787bece7225ac57f789ced36984394066c5382d9
Vlad Crosman <vlad.crosman@ro.ibm.com>
2020-12-04 20:33:36 +0200

    back to http

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const logger = require('./server/utils/logger');
 const dbConnect = require('./server/utils/dbConnector').connect;
 const app = require('./app');
-const https = require('https');
-const fs = require('fs');
-const privateKey = fs.readFileSync('./privatekey.pem');
-const certificate = fs.readFileSync('./server.crt');
+// const https = require('https');
+// const fs = require('fs');
+// const privateKey = fs.readFileSync('./privatekey.pem');
+// const certificate = fs.readFileSync('./server.crt');
 
-var credentials = {key: privateKey, cert: certificate};
+// var credentials = {key: privateKey, cert: certificate};
 
-var httpsServer = https.createServer(credentials, app);
+// var httpsServer = https.createServer(credentials, app);
 
 // Connect to database
 dbConnect(() => {
 
     // On connect start web server
-    httpsServer.listen(app.get('port'), '0.0.0.0', () => {
+    app.listen(app.get('port'), '0.0.0.0', () => {
         logger.info(`Microservice listening at port ${app.get('port')} in ${process.env.NODE_ENV || 'development'}`);
     });
 
